fd7f995c21909651a77a0e5bf1516ec576f40cfa
fabricio <furimatarrita15@gmail.com>
2023-02-06 13:37:13 -0600

    Pagina Tarea

---
+++ src/main/java/com/Practica2/Pagina_Ejemplo/PaginaEjemploApplication.java
@@ -0,0 +1,13 @@
+package com.Practica2.Pagina_Ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaEjemploApplication.class, args);
+	}
+
+}
